5bf9d281d7500c0d5616121dd7a9d32813909ff6
Stephanie-Y-Lopez <145513219+Stephanie-Y-Lopez@users.noreply.github.com>
2024-05-06 16:07:10 -0700

    Update now only 1 error
    Only 1 error left in the whole script even with the "OnCollisionEnter2D" method commented out, I just misspelled causing other errors when commenting out that method. The only left is "Assets\Scripts\ContactWithGround.cs(34,43): error CS1002: ; expected"

--- Assets/Scripts/ContactWithGround.cs
+++ Assets/Scripts/ContactWithGround.cs
@@ -12,37 +12,37 @@ public class ContactWithGround : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigibody2D>();
-        jumpForce = 5;
+        rb = GetComponent<Rigidbody2D>();
+        jumpForce = 50;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(input.GetButtonDown("jump"))
+        if (Input.GetButtonDown("jump"))
         {
-            if(isGrounded)
+            if (isGrounded)
             {
-                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impluse);
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             }
         }
     }
 
-    //private void OnCollisionEnter2D(Collision2D other)
-    //{
-    //    // Gets the layer of the item you collide with as an int
-    //    int 6 = other.gameObject.layer;
-    //    // Converts that number to the layer name
-    //    string Ground = LayerMask.LayerToName(6);
-    //    // This is the name we want to respond to
-    //    string groundLayerName = "Ground";
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // Gets the layer of the item you collide with as an int
+        int layerNumber = other.gameObject.6;
+        // Converts that number to the layer name
+        string layerName = LayerMask.LayerToName(6);
+        // This is the name we want to respond to
+        string groundLayerName = "Ground";
 
-    //    // Checks if the layer is a ground. Set to true if it's not
-    //    if (Ground == groundLayerName)
-    //    {
-    //        isGrounded = true;
-    //    }
-    //}
+        // Checks if the layer is a ground. Set to true if it's not
+        if (Ground == groundLayerName)
+        {
+            isGrounded = true;
+        }
+    }
 
     private void OnCollisionExit2D(Collision2D other)
     {
